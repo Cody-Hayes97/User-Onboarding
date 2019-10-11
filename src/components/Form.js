af7dbe163c678074b11680b97b7d10f8371300af
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-10-10 19:07:23 -0700

    validation and submitting

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+import Error from "./Error";
 
 export default function UserForm(props) {
-  const [users, setUser] = setState([]);
+  const [users, setUser] = useState([]);
 
   const validationSchema = Yup.object().shape({
     name: Yup.string()
@@ -16,30 +17,64 @@ export default function UserForm(props) {
       .required("Must enter an email"),
     password: Yup.string()
       .min(8, "password is too short, should be at least 8 characters")
-      .required("Must enter a password")
+      .required("Must enter a password"),
+    checkbox: Yup.bool().required("please agree to the terms and conditions")
   });
 
   return (
-    <Formik>
-      <Form>
-        <Field type="text" name="name" placeholder="Enter your name" />
-        <Field type="email" name="email" placeholder="Enter your email" />
-        <Field
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-        <label htmlfor="check-box">
-          Terms of Service
+    <Formik
+      initialValues={{ name: "", email: "", password: "", checkbox: false }}
+      validationSchema={validationSchema}
+    >
+      {({ values, errors, touched, handleChange, handleBlur }) => (
+        <Form onSubmit={handleSubmit}>
           <Field
-            id="check-box"
-            type="checkbox"
+            type="text"
             name="name"
             placeholder="Enter your name"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={touched.name && errors.name ? "has-error" : null}
           />
-        </label>
-        <button type="submit">Submit</button>
-      </Form>
+          <Error touched={touched.name} message={errors.name} />
+          <Field
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={touched.email && errors.email ? "has-error" : null}
+          />
+          <Error touched={touched.email} message={errors.email} />
+          <Field
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={touched.password && errors.password ? "has-error" : null}
+          />
+          <Error touched={touched.password} message={errors.password} />
+          <label htmlfor="check-box">
+            Terms of Service
+            <Field
+              id="check-box"
+              type="checkbox"
+              name="checkbox"
+              placeholder="Enter your name"
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </Form>
+      )}
     </Formik>
   );
 }
+
+const handleSubmit = e => {
+  e.preventDefault();
+  console.log("submitted!");
+};
