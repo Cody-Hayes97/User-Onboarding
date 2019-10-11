d9d03661fb8610293a2013ce402c640360c04473
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-10-10 20:35:19 -0700

    almost displaying data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import Error from "./Error";
+import UserData from "./DisplayUsers";
 
-export default function UserForm(props) {
+export default function UserForm() {
   const [user, setUser] = useState([]);
 
   console.log(user);
 
-  const handleSubmit = (e, values) => {
-    e.preventDefault();
-    console.log("submitted!");
-    axios
-      .post("https://reqres.in/api/users", ...values)
-      .then(response => {
-        console.log(response);
-        setUser(response.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  //   useEffect(() => {
+  //     if (props.status) {
+  //       setUser([...user, props.status]);
+  //     }
+  //   }, [props.status]);
+
+  //   const handleSubmit = (e, values) => {
+  //     e.preventDefault();
+  //     console.log("submitted!");
+  //     axios
+  //       .post("https://reqres.in/api/users", e.target.value)
+  //       .then(response => {
+  //         console.log(response);
+  //         setUser(response.data);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       });
+  //   };
 
   const validationSchema = Yup.object().shape({
     name: Yup.string()
@@ -38,54 +45,83 @@ export default function UserForm(props) {
   });
 
   return (
-    <Formik
-      initialValues={{ name: "", email: "", password: "", checkbox: false }}
-      validationSchema={validationSchema}
-    >
-      {({ values, errors, touched, handleChange, handleBlur }) => (
-        <Form onSubmit={handleSubmit}>
-          <Field
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={touched.name && errors.name ? "has-error" : null}
-          />
-          <Error touched={touched.name} message={errors.name} />
-          <Field
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={touched.email && errors.email ? "has-error" : null}
-          />
-          <Error touched={touched.email} message={errors.email} />
-          <Field
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={touched.password && errors.password ? "has-error" : null}
-          />
-          <Error touched={touched.password} message={errors.password} />
-          <label htmlFor="check-box">
-            Terms of Service
+    <div>
+      <Formik
+        initialValues={{ name: "", email: "", password: "", checkbox: false }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          axios
+            .post("https://reqres.in/api/users", values)
+            .then(response => {
+              console.log(response);
+              setUser(response.data);
+              setSubmitting(false);
+              resetForm();
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+        }) => (
+          <Form onSubmit={handleSubmit}>
             <Field
-              id="check-box"
-              type="checkbox"
-              name="checkbox"
+              type="text"
+              name="name"
               placeholder="Enter your name"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={touched.name && errors.name ? "has-error" : null}
             />
-          </label>
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
+            <Error touched={touched.name} message={errors.name} />
+            <Field
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={touched.email && errors.email ? "has-error" : null}
+            />
+            <Error touched={touched.email} message={errors.email} />
+            <Field
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                touched.password && errors.password ? "has-error" : null
+              }
+            />
+            <Error touched={touched.password} message={errors.password} />
+            <label htmlFor="check-box">
+              Terms of Service
+              <Field
+                id="check-box"
+                type="checkbox"
+                name="checkbox"
+                placeholder="Enter your name"
+              />
+            </label>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+      <UserData userList={user} />
+    </div>
   );
 }
